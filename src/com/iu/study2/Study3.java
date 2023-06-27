1c7b0e45b94b0aa12a093859ab313ee0d763a2ce
sdbsgh98 <songyunho98@gmail.com>
2023-06-27 17:46:07 +0900

    20230627 수업

---
+++ src/com/iu/study2/Study3.java
@@ -0,0 +1,32 @@
+package com.iu.study2;
+
+public class Study3 {
+
+	public static void main(String[] args) {
+		//if, if-else, if -elseif -else
+		//switch() case
+		int num = 20;
+		//int check1 = 10; case문에서 불가능하다.
+		
+		switch(num*2) {
+		case 10:
+			System.out.println("num 10");
+			break;
+		case 20:
+			System.out.println("num 20");
+			break;
+		default:
+			System.out.println("num 모름");
+		}
+		
+		if(num*2 == 10) {
+			
+		}else if(num*2 == 20) {
+			
+		}else {
+			
+		}
+
+	}
+
+}
