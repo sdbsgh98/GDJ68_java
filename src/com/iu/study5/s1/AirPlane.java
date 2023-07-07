cf10de8dd7e2a6bcb7d259d21ade671fd5cfd34d
sdbsgh98 <songyunho98@gmail.com>
2023-07-07 17:06:03 +0900

    20230707 수업

--- src/com/iu/study5/s1/AirPlane.java
+++ src/com/iu/study5/s1/AirPlane.java
@@ -1,5 +1,12 @@
 package com.iu.study5.s1;
 
 public class AirPlane implements Flyable {
+	
+	//오버라이딩
+	@Override
+	public void fly() {
+		
+		
+	}
 
 }
---
+++ src/com/iu/study6/ApiMain.java
@@ -0,0 +1,16 @@
+package com.iu.study6;
+
+public class ApiMain {
+
+	public static void main(String[] args) {
+		//객체 생성
+		Object obj = new Object();
+		boolean check = obj.equals(null);
+		String str = obj.toString();
+		int r = obj.hashCode();
+		String st = "hello";
+		char ch = st.charAt(1);
+		System.out.println(ch);
+	}
+
+}
